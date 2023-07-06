c274cb5b8da7cda2fc38d64e7fa997cec163baee
Julio Rodriguez <julio.rodriguezr1@gmail.com>
2023-07-06 16:32:58 -0400

    restoring handleChangeClinic

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -36,7 +36,7 @@ import LanguageContext from '../contexts/LanguageContext';
 import { useTranslation } from 'react-i18next';
 import './../App.css'
 import { useSelector } from 'react-redux';
-import {handleChangeClinics} from './../utils/functions'
+// import {handleChangeClinics} from './../utils/functions'
 
 
 export default function Test(props) {
@@ -150,6 +150,48 @@ export default function Test(props) {
         setSpecialities(specialities_en);
         break;
     }
+  };
+
+  const handleChangeClinics = (event) => {
+
+    setMapWidth('17vw');
+    if (event.target.value !== "") {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        const nameOfClinicValue = getValues('nameOfClinic');
+
+        let url = `${myProxy}https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${nameOfClinicValue}&inputtype=textquery&fields=name,formatted_address,rating,opening_hours,geometry,place_id&key=${apiKey}`;
+        console.log('url', url)
+        fetch(url)
+          .then((nameOfClinicFetchResponse) => {
+            return nameOfClinicFetchResponse.json();
+          }).then((nameOfClinicFetchResponseJson) => {
+            setClinicsToDisplay([])
+            let data = nameOfClinicFetchResponseJson.candidates[0];
+            let lat = data.geometry.location.lat;
+            let lng = data.geometry.location.lng;
+            let name = data.name;
+            let openNow = data.opening_hours.open_now;
+            let address = data.formatted_address;
+            let rating = data.rating;
+            let phone = 333444555;
+            let photos = data.photos;
+            let id = data.place_id || 1;
+            let placeId = data.place_id;
+            let clinicToDisplayObj = { lat, lng, name, openNow, address, rating, phone, photos, id, placeId, data }
+            console.log('clinicToDisplayObj', clinicToDisplayObj)
+            setClinicsToDisplay([clinicToDisplayObj]);
+            reset({
+              'country-selected': null,
+              'city-selected': null
+            });
+
+          })
+          .catch((error) => {
+            console.error('Error en la solicitud fetch:', error);
+          });
+      }
+    };
   };
 
   const updateSelectLabels = () => {
