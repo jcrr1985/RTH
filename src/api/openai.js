ce353f334efafeae377109acc1cd636cf3e320fc
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-11-25 19:05:14 +0500

    https

--- src/api/openai.js
+++ src/api/openai.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getDiagnosis = async (symptoms) => {
   try {
     const response = await axios.post(
-      "rth-server-fq5w31u5r-jcrr1985s-projects.vercel.app/api/getDiagnosis",
+      "https://rth-server-fq5w31u5r-jcrr1985s-projects.vercel.app/api/getDiagnosis",
       {
         symptoms,
       }
